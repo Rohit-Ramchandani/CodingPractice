5597fd3d6dced760b3a2af05baff53d26de6bd7e
Rohit-Ramchandani <38393392+Rohit-Ramchandani@users.noreply.github.com>
2022-08-25 21:07:53 +0530

    Add files via upload

---
+++ 4. Find Anagrams.cpp
@@ -0,0 +1,67 @@
+#include "bits/stdc++.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<int> findAnagrams(string s, string p) 
+{        
+    int i=0;
+    int j=0;        
+    int k = p.size();
+    int ss = s.size();
+    vector<int> ans;
+
+    // Store the frequency of the elements in the map
+    unordered_map <char, int> mpp;
+    for(auto z : p)
+        mpp[z]++;
+
+    int count = mpp.size();
+    while(j < ss)
+    {
+        // If word found in map decreae count
+        if(mpp.find(s[j])!=mpp.end())
+        {
+            // Decrease count in map
+            mpp[s[j]]--;
+                
+            // if count of frequency of the character is equal to 0 decrease count  
+            if(mpp[s[j]] == 0)
+                count--;                   
+        }
+            
+        // Increase window size
+        if((j-i+1) < k) j++;
+            
+        //Window size reached
+        else if((j-i+1) == k)
+        {
+            // All elements found in map
+            if(count == 0)
+                ans.push_back(i);
+            if(mpp.find(s[i])!=mpp.end())
+            {
+                mpp[s[i]]++;
+                if(mpp[s[i]]==1)
+                    count++;
+            }
+            i++;
+            j++;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+	string s = "cbaebabacd";
+	string p = "abc";
+	
+	vector<int> ans;
+	ans = findAnagrams(s,p);
+	for(int i=0; i<ans.size();i++)
+	{
+		cout << ans[i];
+	}
+	return 0;
+}
